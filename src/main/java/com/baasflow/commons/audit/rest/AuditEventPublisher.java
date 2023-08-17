8e54d920407b40094e061339c0b0071082b520ab
Kristof Jozsa <kristof.jozsa@gmail.com>
2023-08-17 15:50:11 +0200

    fix rat setup for baasflow's apache license

--- src/main/java/com/baasflow/commons/audit/rest/AuditEventPublisher.java
+++ src/main/java/com/baasflow/commons/audit/rest/AuditEventPublisher.java
@@ -6,6 +6,7 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this  file except in compliance
  * with the License. You may obtain a copy of the License at
+ * 
  * http://www.baasFlow.com/licenses/Apache_LICENSE-2.0
  * or the root of this project.
  *
