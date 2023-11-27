29fe122b11719a9a5cdcfe82c4665e27e3242d3b
Kristof Jozsa <kristof.jozsa@gmail.com>
2023-11-27 11:37:07 +0100

    kafka healthcheck implemented

--- src/main/java/com/baasflow/commons/events/internal/KafkaHealthIndicator.java
+++ src/main/java/com/baasflow/commons/events/internal/KafkaHealthIndicator.java
@@ -27,7 +27,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.concurrent.atomic.AtomicReference;
 
-@Component
+@Component("kafka")
 public class KafkaHealthIndicator implements HealthIndicator {
     private final AtomicReference<Health> healthStatus = new AtomicReference<>(Health.unknown().build());
 
